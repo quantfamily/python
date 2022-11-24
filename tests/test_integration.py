3e5c429786444d04f732a8bfc2acac59f48828d3
Henrik Nilsson <henrik@lhjnilsson.com>
2022-11-24 20:39:43 +0100

    boilerplate

---
+++ tests/test_integration.py
@@ -0,0 +1,44 @@
+import pytest
+from typing import Iterator
+
+from foreverbull_zipline.models import IngestConfig, EngineConfig
+from foreverbull_core.models.socket import Request
+
+class Backtest:
+    def __init__(self):
+        pass
+
+    def ingest(self, config: IngestConfig) -> None:
+        pass
+
+    def configure(self, config: EngineConfig) -> None:
+        pass
+
+    def run(self) -> Iterator[Request]:
+        pass
+
+class Client:
+    def __init__(self):
+        pass
+
+    def configure(self) -> None:
+        pass
+
+    def process(self, request: Request) -> None:
+        pass
+
+@pytest.fixture
+def backtest():
+    pass
+
+@pytest.fixture
+def client():
+    pass
+
+
+def test_backtest_client_connection(backtest: Backtest, client: Client):
+    backtest.configure()
+    client.configure()
+
+    for message in backtest.run():
+        client.process(message)
